af091c2750be1725e83f5175f25f99dc2246e542
Morgan Stanley <morganpstanley@gmail.com>
2019-07-02 04:18:22 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  if text.scan(/\w{5}/)
+  if text.grep(/\w{5}/)
     return true
   else
     return false
