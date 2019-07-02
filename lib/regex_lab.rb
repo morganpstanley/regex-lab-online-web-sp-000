a2e62d0f41a28699950a77f9f9b684077d0302ea
Morgan Stanley <morganpstanley@gmail.com>
2019-07-02 05:10:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text.grep(/\^un\w{0,5}ing/)
+  if text.scan(/\^un\w{0,5}ing/)
     return text
   else
     return false
